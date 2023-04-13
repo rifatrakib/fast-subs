61e5bd96146bd7959724059b87f975426eba8c3e
rifatrakib <Rakib.1508@outlook.com>
2023-04-13 15:20:45 +0600

    modified gateway app with new template

--- server/apps/gateway/server/main.py
+++ server/apps/gateway/server/main.py
@@ -6,6 +6,14 @@ from .docs.manager import retrieve_api_metadata, retrieve_tags_metadata
 app = FastAPI(**retrieve_api_metadata(), openapi_tags=retrieve_tags_metadata())
 
 
+def customize_operation_ids(app: FastAPI) -> None:
+    for route in app.routes:
+        route.operation_id = f"{app.title}_{route.name}"
+
+
+customize_operation_ids(app)
+
+
 @app.get("/health")
 async def health_check():
     return settings
